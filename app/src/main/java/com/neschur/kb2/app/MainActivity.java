87b95d7b902b1baed8d1304b2cd19ae3eb56ce9e
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2014-06-02 21:07:35 +0300

    Create field from images

--- app/src/main/java/com/neschur/kb2/app/MainActivity.java
+++ app/src/main/java/com/neschur/kb2/app/MainActivity.java
@@ -4,14 +4,36 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 
 public class MainActivity extends Activity {
+    private ImageView[][] images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        RelativeLayout linLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams linLayoutParam = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        setContentView(linLayout, linLayoutParam);
+
+        images = new ImageView[6][5];
+
+        for(int x=0;x<6; x++) {
+            for (int y = 0; y < 5; y++) {
+                RelativeLayout.LayoutParams lpView = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+                lpView.leftMargin = 160 * x;
+                lpView.topMargin = 120 * y;
+                images[x][y] = new ImageView(this);
+                images[x][y].setImageResource(R.drawable.ic_launcher);
+                images[x][y].setLayoutParams(lpView);
+                linLayout.addView(images[x][y]);
+            }
+        }
     }
 
 
