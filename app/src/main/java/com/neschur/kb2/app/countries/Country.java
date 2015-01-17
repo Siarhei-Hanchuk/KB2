555d6267e2fb1d1db948e4a60d0fd182205cad38
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-01-17 22:11:58 +0300

    Refactor Country

--- app/src/main/java/com/neschur/kb2/app/countries/Country.java
+++ app/src/main/java/com/neschur/kb2/app/countries/Country.java
@@ -17,13 +17,40 @@ public abstract class Country {
     public final static int MAX_MAP_SIZE = 65;
     protected MapPoint[][] map;
     protected int id;
+    protected Random random;
+
+    public Country(int id) {
+        this.id = id;
+        this.random = new Random();
+
+        map = new MapPoint[65][65];
+        for (int i = 0; i < 65; i++) {
+            for (int j = 0; j < 65; j++) {
+                map[i][j] = new MapPoint();
+            }
+        }
+
+        base();
+        guidePosts();
+        cities();
+
+        new Magician(this, 5, 8); //debug
+    }
+
+    protected abstract int goldChestMin();
+
+    protected abstract int goldChestMax();
 
     protected int rand(int n) {
-        return Math.abs(new Random().nextInt()) % n;
+        return Math.abs(random.nextInt()) % n;
     }
 
     public MapPoint getMapPoint(int x, int y) {
         return map[x][y];
+    }
+
+    public int getId() {
+        return id;
     }
 
     private void base() {
@@ -38,36 +65,6 @@ public abstract class Country {
             }
         }
     }
-
-//    public boolean worker(int n, int x, int y) {
-//        int T1 = 0;
-//        int T2 = 0;
-//        switch (n) {
-//            case 2:
-//                T1 = R.drawable.forest;
-//                T2 = R.drawable.land;
-//                break;
-//            case 4:
-//                T1 = R.drawable.stone;
-//                T2 = R.drawable.land;
-//                break;
-//            case 3:
-//                T1 = R.drawable.land;
-//                T2 = R.drawable.water;
-//                break;
-//            case 1:
-//                T1 = R.drawable.water;
-//                T2 = R.drawable.plot;
-//                break;
-//            default:
-//                break;
-//        }
-//        if (map[x][y].land == T1) {
-//            map[x][y].land = T2;
-//            return true;
-//        }
-//        return false;
-//    }
 
     protected void cities() {
         map[6][5].setEntity(new City(this, 6, 5));
@@ -99,15 +96,10 @@ public abstract class Country {
         }
     }
 
-    protected abstract int goldChestMin();
-
-    protected abstract int goldChestMax();
-
     protected void goldChests(int frequency) {
         for (int i = 5; i < MAX_MAP_SIZE - 5; i++) {
             for (int j = 5; j < MAX_MAP_SIZE - 5; j++) {
                 if (((map[i][j].land == R.drawable.land) || (map[i][j].land == R.drawable.sand)) && (map[i][j].getEntity() == null)) {
-                    ;
                     if (rand(frequency) == 1) {
                         map[i][j].setEntity(new GoldChest(this, i, j, goldChestMin(), goldChestMax()));
                     }
@@ -116,84 +108,40 @@ public abstract class Country {
         }
     }
 
-	/*private void castels(){
-        int x=0;
-		int y=0;
-		int c=0;
-		while(c<5){
-			x=rand(MAX_MAP_SIZE);
-			y=rand(MAX_MAP_SIZE);
-			if(((map[x][y].land==R.drawable.land)&&(map[x-1][y].land==R.drawable.land)&&(map[x+1][y].land==R.drawable.land)&&(map[x][y+1].land==R.drawable.land))&&
-					((map[x][y].obj==0)&&(map[x-1][y].obj==0)&&(map[x+1][y].obj==0)&&(map[x][y+1].obj==0))){
-				map[x][y].obj=R.drawable.castel_c;
-				map[x+1][y].obj=R.drawable.castel_r;
-				map[x-1][y].obj=R.drawable.castel_l;
-				map[x][y].addid=c;
-				map[x][y+1].obj=R.drawable.capitan;
-				map[x][y+1].addid=c;
-				capitan(c);
-				c++;
-			}
-		}
-	}*/
-
-    //private void capitan(int cid){}
-
-	/*@SuppressWarnings("unused")
-    private void capitans(){
-		int x;
-		int y;
-		int c=5;
-		while(c<20){
-			x=rand(56)+4;
-			y=rand(56)+4;
-			if(map[x][y].land==R.drawable.land && map[x][y].obj==0){
-				map[x][y].obj=R.drawable.capitan;
-				capitan(c);
-				c++;
-			}
-		}
-	}*/
-
     private void createRiver(int x, int y, int direction, int length, int type) {
-        int p = 0;
-        int q = 0;
-        int w = 0;
-        int o1 = 0;
-        int o2 = 0;
-        while (p < length) {
-            p++;
-            q = rand(2);
-            q = rand(3);
-            for (int o = -q - 1; o < w + 2; o++) {
+        int run = 0;
+        int width;
+        while (run < length) {
+            run++;
+            width = rand(3);
+            for (int coordinate = -width - 1; coordinate < +width + 1; coordinate++) {
                 if (type == 1) {
-                    o2 = o;
+                    map[x][y + coordinate].land = R.drawable.water;
                 } else {
-                    o1 = o;
+                    map[x + coordinate][y].land = R.drawable.water;
                 }
-                map[x + o1][y + o2].land = R.drawable.water;
             }
-            int e;
-            int r;
+            int center;
+            int route;
             if (type == 1) {
-                e = y;
-                r = x;
+                center = y;
+                route = x;
             } else {
-                e = x;
-                r = y;
+                center = x;
+                route = y;
             }
-            e = e + rand(5) - 2;
-            if (e > 62)
-                e = 62;
-            if (e < 3)
-                e = 3;
-            r += direction;
+            center = center + rand(5) - 2;
+            if (center > 62)
+                center = 62;
+            if (center < 3)
+                center = 3;
+            route += direction;
             if (type == 1) {
-                y = e;
-                x = r;
+                y = center;
+                x = route;
             } else {
-                x = e;
-                y = r;
+                x = center;
+                y = route;
             }
         }
     }
@@ -241,23 +189,70 @@ public abstract class Country {
         map[x][y].setEntity(new MapNext(this, x, y));
     }
 
-    public Country(int id) {
-        this.id = id;
-        map = new MapPoint[65][65];
-        for (int i = 0; i < 65; i++) {
-            for (int j = 0; j < 65; j++) {
-                map[i][j] = new MapPoint();
-            }
-        }
-
-        base();
-        guidePosts();
-        cities();
-        new Magician(this, 5, 8); //debug
-    }
-
-    public int getId() {
-        return id;
-    }
+//    private void castels() {
+//        int x = 0;
+//        int y = 0;
+//        int c = 0;
+//        while (c < 5) {
+//            x = rand(MAX_MAP_SIZE);
+//            y = rand(MAX_MAP_SIZE);
+//            if (((map[x][y].land == R.drawable.land) && (map[x - 1][y].land == R.drawable.land) && (map[x + 1][y].land == R.drawable.land) && (map[x][y + 1].land == R.drawable.land)) &&
+//                    ((map[x][y].obj == 0) && (map[x - 1][y].obj == 0) && (map[x + 1][y].obj == 0) && (map[x][y + 1].obj == 0))) {
+//                map[x][y].obj = R.drawable.castel_c;
+//                map[x + 1][y].obj = R.drawable.castel_r;
+//                map[x - 1][y].obj = R.drawable.castel_l;
+//                map[x][y].addid = c;
+//                map[x][y + 1].obj = R.drawable.capitan;
+//                map[x][y + 1].addid = c;
+//                capitan(c);
+//                c++;
+//            }
+//        }
+//    }
+//
+//    private void capitans() {
+//        int x;
+//        int y;
+//        int c = 5;
+//        while (c < 20) {
+//            x = rand(56) + 4;
+//            y = rand(56) + 4;
+//            if (map[x][y].land == R.drawable.land && map[x][y].obj == 0) {
+//                map[x][y].obj = R.drawable.capitan;
+//                capitan(c);
+//                c++;
+//            }
+//        }
+//    }
+//
+//    public boolean worker(int n, int x, int y) {
+//        int T1 = 0;
+//        int T2 = 0;
+//        switch (n) {
+//            case 2:
+//                T1 = R.drawable.forest;
+//                T2 = R.drawable.land;
+//                break;
+//            case 4:
+//                T1 = R.drawable.stone;
+//                T2 = R.drawable.land;
+//                break;
+//            case 3:
+//                T1 = R.drawable.land;
+//                T2 = R.drawable.water;
+//                break;
+//            case 1:
+//                T1 = R.drawable.water;
+//                T2 = R.drawable.plot;
+//                break;
+//            default:
+//                break;
+//        }
+//        if (map[x][y].land == T1) {
+//            map[x][y].land = T2;
+//            return true;
+//        }
+//        return false;
+//    }
 }
 
