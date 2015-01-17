67f0ebe6b667acbfe199cddbc03620de47d04699
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-01-17 18:26:44 +0300

    Update MagicianMenu

--- app/src/main/java/com/neschur/kb2/app/ui/MagicianMenu.java
+++ app/src/main/java/com/neschur/kb2/app/ui/MagicianMenu.java
@@ -10,7 +10,11 @@ import com.neschur.kb2.app.entities.Entity;
  * Created by siarhei on 12.1.15.
  */
 public class MagicianMenu extends Menu {
-    final static int COUNT = 6;
+    private final int COUNT = 6;
+    private final int PRICE_MAGIC_POWER = 5000;
+    private final int PRICE_WORKERS = 1000;
+    private final int PRICE_MOVE_TO_COUNTRY = 5000;
+    private final int PRICE_TORNADO = 1000;
 
     public MagicianMenu(Activity activity, Entity magician, GameController gameController) {
         super(activity, gameController);
@@ -18,32 +22,57 @@ public class MagicianMenu extends Menu {
 
     @Override
     public String getItemDescription(int i) {
-        switch (i) {
+        switch (menuMode) {
             case 0:
-                return resources.getString(R.string.entity_menus_magician_item1);
+                switch (i) {
+                    case 0:
+                        return menuItem(R.string.entity_menus_magician_item1, PRICE_WORKERS);
+                    case 1:
+                        return menuItem(R.string.entity_menus_magician_item2, PRICE_MAGIC_POWER);
+                    case 2:
+                        return menuItem(R.string.entity_menus_magician_item3, PRICE_MOVE_TO_COUNTRY);
+                    case 3:
+                        return menuItem(R.string.entity_menus_magician_item4);
+                    case 4:
+                        return menuItem(R.string.entity_menus_magician_item5, PRICE_TORNADO);
+                    case 5:
+                        return "-";
+                    default:
+                        return null;
+                }
             case 1:
-                return resources.getString(R.string.entity_menus_magician_item2);
-            case 2:
-                return "-";
-            case 3:
-                return "-";
-            case 4:
-                return "-";
-            case 5:
-                return "-";
-            default:
-                return null;
+                switch (i) {
+                    case 0:
+                        return resources.getString(R.string.entity_menus_city_workers_item1);
+                    case 1:
+                        return resources.getString(R.string.entity_menus_city_workers_item2);
+                    case 2:
+                        return resources.getString(R.string.entity_menus_city_workers_item3);
+                    case 3:
+                        return resources.getString(R.string.entity_menus_city_workers_item4);
+                }
         }
+        return null;
     }
 
     @Override
     public boolean select(int i) {
-        switch (i) {
+        switch (menuMode) {
             case 0:
-                player.upUsedMagicianCount();
-                return true;
+                switch (i) {
+                    case 0:
+                        menuMode = 1;
+                        return false;
+                    case 1:
+                        if (player.changeMoney(-PRICE_MAGIC_POWER)) {
+                            player.upMagicPower();
+                            player.upUsedMagicianCount();
+                        }
+                        return true;
+                }
             case 1:
-                player.upMagicPower();
+                player.changeMoney(-PRICE_WORKERS);
+                player.changeWorker(i, +player.getMagicPower());
                 player.upUsedMagicianCount();
                 return true;
         }
