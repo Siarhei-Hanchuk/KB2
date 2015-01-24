a54c418495db7c18204eb504bb06dd2d2d4564c9
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-01-24 15:48:39 +0300

    Images for all army

--- app/src/main/java/com/neschur/kb2/app/warriors/WarriorFactory.java
+++ app/src/main/java/com/neschur/kb2/app/warriors/WarriorFactory.java
@@ -54,35 +54,35 @@ public class WarriorFactory {
             case "rknight":
                 return new Warrior(id, R.drawable.army_rknight, 25, 8, 1, false, false, 200, 300);
             case "druid":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 60, 22, 3, false, true, 100, 500);
+                return new Warrior(id, R.drawable.army_druid, 60, 22, 3, false, true, 100, 500);
             case "cannibal":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 60, 30, 2, false, false, 100, 700);
+                return new Warrior(id, R.drawable.army_cannibal, 60, 30, 2, false, false, 100, 700);
             case "ghost":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 70, 15, 3, false, false, 100, 600);
+                return new Warrior(id, R.drawable.army_ghost, 70, 15, 3, false, false, 100, 600);
             case "lion":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 70, 22, 4, false, false, 100, 800);
+                return new Warrior(id, R.drawable.army_lion, 70, 22, 4, false, false, 100, 800);
             case "snake":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 70, 65, 1, false, false, 30, 1500);
+                return new Warrior(id, R.drawable.army_snake, 70, 65, 1, false, false, 30, 1500);
             case "troll":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 100, 25, 1, false, false, 100, 1000);
+                return new Warrior(id, R.drawable.army_troll, 100, 25, 1, false, false, 100, 1000);
             case "vampire":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 110, 40, 1, true, false, 50, 2000);
+                return new Warrior(id, R.drawable.army_vampire, 110, 40, 1, true, false, 50, 2000);
             case "giant":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 120, 45, 2, false, true, 50, 3000);
+                return new Warrior(id, R.drawable.army_giant, 120, 45, 2, false, true, 50, 3000);
             case "elephant":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 130, 50, 1, false, false, 50, 2000);
+                return new Warrior(id, R.drawable.army_elephant, 130, 50, 1, false, false, 50, 2000);
             case "knight":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 150, 55, 1, false, false, 70, 3000);
+                return new Warrior(id, R.drawable.army_knight, 150, 55, 1, false, false, 70, 3000);
             case "centaur":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 400, 200, 3, false, true, 30, 5000);
+                return new Warrior(id, R.drawable.army_centaur, 400, 200, 3, false, true, 30, 5000);
             case "dinosaur":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 450, 200, 2, false, false, 50, 5000);
+                return new Warrior(id, R.drawable.army_dinosaur, 450, 200, 2, false, false, 50, 5000);
             case "daemon":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 500, 200, 1, true, false, 30, 7000);
+                return new Warrior(id, R.drawable.army_daemon, 500, 200, 1, true, false, 30, 7000);
             case "cyclops":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 600, 400, 2, false, false, 30, 8000);
+                return new Warrior(id, R.drawable.army_cyclops, 600, 400, 2, false, false, 30, 8000);
             case "dragon":
-                return new Warrior(id, R.drawable.status_ancientmap_0, 1000, 250, 1, true, false, 30, 10000);
+                return new Warrior(id, R.drawable.army_dragon, 1000, 250, 1, true, false, 30, 10000);
             default:
                 return null;
         }
