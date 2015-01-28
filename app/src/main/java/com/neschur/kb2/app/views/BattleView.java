19d65c3dac480304d676357dde666f58a25cef2a
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2015-01-28 15:34:37 +0300

    Display count of warriors squad in battle

--- app/src/main/java/com/neschur/kb2/app/views/BattleView.java
+++ app/src/main/java/com/neschur/kb2/app/views/BattleView.java
@@ -3,7 +3,9 @@ package com.neschur.kb2.app.views;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
@@ -39,6 +41,10 @@ public class BattleView extends View {
     public void draw(@NonNull Canvas canvas) {
         ImageCache imageCache = ImageCache.getInstance(getResources(), stepX(), stepY());
         calcOffsets();
+        Paint countPaint = getDefaultPaint();
+        countPaint.setTextSize(textHeight() / 3);
+        Paint countPaintBg = getDefaultPaint();
+        countPaintBg.setColor(Color.BLACK);
 
         MapPointBattle[][] map = battleController.getMap();
         for (int x = 0; x < 6; x++) {
@@ -55,6 +61,17 @@ public class BattleView extends View {
                         image = flipImage(image);
                     }
                     canvas.drawBitmap(image, xOffset + x * stepX(), yOffset + y * stepY(), null);
+                    canvas.drawRect(xOffset + x * stepX(),
+                            yOffset + y * stepY() + stepY() - textHeight() / 3,
+                            xOffset + x * stepX() +
+                                    countPaint.measureText(
+                                            Integer.toString(warrior.getCount())) + 4,
+                            yOffset + y * stepY() + stepY(),
+                            countPaintBg);
+                    canvas.drawText(Integer.toString(warrior.getCount()),
+                            xOffset + x * stepX() + 2,
+                            yOffset + y * stepY() + stepY() - 2,
+                            countPaint);
                 }
             }
         }
